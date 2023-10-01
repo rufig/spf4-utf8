373323593a9ae7fbfcc93689a6f17dba45a4090d
ruv <ruv@users.noreply.github.com>
2023-10-01 06:29:43 +0400

    FIX src posix -- RWX memory for code space
    In new versions of Linux, depending on the BIOS setup and system
    configuration, the memory regions allocated for code must be explicitly
    configured as executable.
    For example, see https://wiki.ubuntu.com/Security/Features#nx
    
    For this reason, a new allocation method, `ALLOCATE-RWX`, has been
    introduced. This method is now used to allocate memory for dynamically
    allocated dictionaries and temporary wordlists.
    
    Thanks to @ivanivanovo for reporting of the initial problem
    and research for possible solutions: https://t.me/spforthchat/764

--- src/posix/memory.f
+++ src/posix/memory.f
@@ -21,11 +21,11 @@ VARIABLE USER-OFFS \ смещение в области данных поток�
 
 VARIABLE calloc-adr
 
-: errno ( -- n)
+: errno ( -- n )
   (()) __errno_location @
 ;
 
-: ?ERR ( -1/n -- x err / n 0)
+: ?ERR ( -1 -- -1 err | x -- x 0 )
   DUP -1 = IF errno ELSE 0 THEN
 ;
 
@@ -66,6 +66,10 @@ VARIABLE EXTRA-MEM
 
 : FIX-MEMTAG ( addr-allocated -- ) (FIX-MEMTAG) DROP ;
 
+: ADD-SIZE ( u1 u2 -- u3 0 | u1 ior )
+  2DUP 1+ NEGATE U< IF + 0 EXIT THEN DROP -24 \ "invalid numeric argument"
+;
+
 : ALLOCATE ( u -- a-addr ior ) \ 94 MEMORY
 \ Распределить u байт непрерывного пространства данных. Указатель пространства 
 \ данных не изменяется этой операцией. Первоначальное содержимое выделенного 
@@ -80,10 +84,10 @@ VARIABLE EXTRA-MEM
 \ по умолчанию заполняется адресом тела процедуры, вызвавшей ALLOCATE
 
   \ Сразу возвратить ошибку, если добавление служебной ячейки даст переполнение
-  DUP [ 1 CELLS 1+ NEGATE ] LITERAL U> IF DROP 0 -300 EXIT THEN
+  CELL ADD-SIZE DUP IF EXIT THEN DROP ( u2 )
 
-  CELL+ 1 SWAP 2 calloc-adr @ C-CALL
-  DUP IF R@ OVER ! CELL+ ( ~~ FIX-MEMTAG ) 0 ELSE -300 THEN
+  1 SWAP 2 calloc-adr @ C-CALL
+  DUP IF CELL+ (FIX-MEMTAG) 0 EXIT THEN -300
 ;
 
 : FREE ( a-addr -- ior ) \ 94 MEMORY
@@ -113,4 +117,30 @@ VARIABLE EXTRA-MEM
 \ изменяется, и ior - зависящий от реализации код ввода-вывода.
   CELL+ SWAP CELL- SWAP 2 realloc-adr @ C-CALL
   DUP IF CELL+ 0 ELSE -300 THEN
+;
+
+
+[UNDEFINED] MEMORY-PAGESIZE [IF]
+  PAGESIZE CONSTANT MEMORY-PAGESIZE
+  \ NB: The "PAGESIZE" word is available only during building,
+  \ and it isn't availabe in the target system.
+[THEN]
+
+: ALLOCATE-RWX ( u -- a-addr 0 | x ior )
+\ Allocate a memory region that can be read, modified, and executed
+  \ add page size (to have at least one page), and one additional cell for MEMTAG
+  MEMORY-PAGESIZE 1- CELL+ ADD-SIZE DUP IF EXIT THEN DROP ( u2 )
+  \ Assertion: pagesize is a power of two, two's complement representation of signed integers
+  MEMORY-PAGESIZE NEGATE AND ( u3 ) \ align u2 down on the page size
+  \ Allocate a range on a pagesize-aligned address, and of pagesize-aligned size
+  \ https://man7.org/linux/man-pages/man3/posix_memalign.3.html
+  >R (( MEMORY-PAGESIZE R@ )) aligned_alloc  ( 0|a-addr1 )
+  \ DUP 0= ?ERR NIP ( 0|a-addr1 ior|0 )
+  DUP 0= -300 AND ( 0|a-addr1 -300|0 )
+  DUP IF NIP R> SWAP ( u3 ior ) EXIT THEN DROP ( a-addr1 )
+  \ Set protection, allow code execution
+  \ https://man7.org/linux/man-pages/man2/mprotect.2.html#EXAMPLES
+  (( DUP  R>  0 PROT_READ OR PROT_WRITE OR PROT_EXEC OR  )) mprotect ?ERR NIP ( a-addr1 ior )
+  DUP IF >R  FREE  R>  ( ior2 ior ) EXIT THEN DROP ( a-addr1 )
+  CELL+ (FIX-MEMTAG) 0 ( a-addr 0 )
 ;
